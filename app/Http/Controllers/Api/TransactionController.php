5131bf5d0a89e4774f59a9bd1301ea99accbddaf
Tio Muhamad Nur <mtio@jakartamrt.co.id>
2024-01-09 00:30:46 +0700

    update

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -86,7 +86,7 @@ class TransactionController extends Controller
             'statusZona' => $statusZona,
         ];
 
-        return response(200)->json($data)->header('Content-Type', 'application/json');
+        return response()->json($data)->header('Content-Type', 'application/json');
     }
 
     public function response(Request $request)
